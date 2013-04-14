2b659e1fa90e15f814713c704010cfeb5150257c
jonathanlermitage <jonathan.lermitage@gmail.com>
2013-04-14 23:18:57 +0200

    the Ant target executor works fine now. TODO: add JaCoCo agent as parameter and load the report Signed-off-by:jonathanlermitage <jonathan.lermitage@gmail.com>

--- tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/EnableCollectionAction.java
+++ tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/EnableCollectionAction.java
@@ -1,18 +1,27 @@
 package fr.tikione.jacocoverage.plugin;
 
+import fr.tikione.jacocoverage.plugin.config.Globals;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.prefs.Preferences;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import org.apache.tools.ant.module.api.AntProjectCookie;
+import org.apache.tools.ant.module.api.AntTargetExecutor;
 import org.netbeans.api.project.Project;
-import org.openide.DialogDisplayer;
-import org.openide.NotifyDescriptor;
+import org.netbeans.spi.project.support.ant.AntProjectHelper;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.awt.DynamicMenuContent;
-import org.openide.filesystems.FileUtil;
+import org.openide.execution.ExecutorTask;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
 import org.openide.util.ContextAwareAction;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
+import org.openide.util.NbPreferences;
 
 @ActionID(category = "Project",
           id = "fr.tikione.jacocoverage.plugin.EnableCollectionAction")
@@ -57,11 +66,25 @@ public final class EnableCollectionAction extends AbstractAction implements Cont
 
         public @Override
         void actionPerformed(ActionEvent e) {
-            String msg = "Project location: " + FileUtil.getFileDisplayName(project.getProjectDirectory());
-//            InputOutput io = IOProvider.getDefault().getIO("JaCoCoverage", true);
-//            io.getOut().println(msg);
-//            io.getOut().close();
-            DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(msg));
+//            String msg = "Project location: " + FileUtil.getFileDisplayName(project.getProjectDirectory());
+            Preferences pref = NbPreferences.forModule(EnableCollectionAction.class);
+            String antTask = pref.get(Globals.PROP_TEST_ANT_TASK, Globals.DEF_TEST_ANT_TASK);
+            String antTaskParams = pref.get(Globals.PROP_TEST_ANT_TASK_PARAMS, Globals.DEF_TEST_ANT_TASK_PARAMS);
+            AntProjectHelper helper = null;
+            Properties targetProps = new Properties();
+            try {
+                FileObject scriptToExecute = project.getProjectDirectory().getFileObject("build", "xml");
+                DataObject dataObj = DataObject.find(scriptToExecute);
+                AntProjectCookie antCookie = dataObj.getLookup().lookup(AntProjectCookie.class);
+                AntTargetExecutor.Env env = new AntTargetExecutor.Env();
+                AntTargetExecutor executor = AntTargetExecutor.createTargetExecutor(env);
+                env.setProperties(targetProps);
+                ExecutorTask task = executor.execute(antCookie, new String[]{antTask});
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (IllegalArgumentException ex) {
+                Exceptions.printStackTrace(ex);
+            }
         }
     }
 }
