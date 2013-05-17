fefa9ec647eac9fe42c88c10c062bd1999b17fda
jonathanlermitage <jonathan.lermitage@gmail.com>
2013-05-17 23:34:56 +0200

    added glyphed annotations support Signed-off-by:jonathanlermitage <jonathan.lermitage@gmail.com>

---
+++ tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/anno/CoverageGlyphedAnnotation.java
@@ -0,0 +1,26 @@
+package fr.tikione.jacocoverage.plugin.anno;
+
+/**
+ * Coverage glyphed annotation.
+ *
+ * @author Jonathan Lermitage
+ */
+public class CoverageGlyphedAnnotation extends CoverageAnnotation {
+
+    private final String desc;
+
+    public CoverageGlyphedAnnotation(CoverageStateEnum state, String projectName, String classFullName, Integer lineNum, String desc) {
+        super(state, projectName, classFullName, lineNum);
+        this.desc = desc;
+    }
+
+    @Override
+    public String getAnnotationType() {
+        return super.getAnnotationType() + "_glyph";
+    }
+
+    @Override
+    public String getShortDescription() {
+        return desc;
+    }
+}
