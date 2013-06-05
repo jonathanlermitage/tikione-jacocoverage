ee12c899544c58fd096189e83470ab16bc87b5fa
jonathanlermitage <jonathan.lermitage@gmail.com>
2013-06-05 16:17:43 +0200

    refactoring and fixed F12 and Alt F12 shortcuts Signed-off-by:jonathanlermitage <jonathan.lermitage@gmail.com>

--- tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/action/ShortcutAntRunProject.java
+++ tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/action/ShortcutAntRunProject.java
@@ -1,72 +1,65 @@
 package fr.tikione.jacocoverage.plugin.action;
 
 import fr.tikione.jacocoverage.plugin.Utils;
-import fr.tikione.jacocoverage.plugin.config.Globals;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import javax.swing.Action;
-import javax.swing.JMenuItem;
 import org.netbeans.api.project.Project;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
-import org.openide.awt.DynamicMenuContent;
 import org.openide.filesystems.FileObject;
 import org.openide.util.ContextAwareAction;
 import org.openide.util.Exceptions;
-import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
-import org.openide.util.LookupEvent;
-import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
-import org.openide.util.actions.Presenter;
 
 /**
- * The "Run with JaCoCoverage" contextual action registration.
+ * The "Run with JaCoCoverage" shortcut action registration.
  * Start the "run" Ant task with the JaCoCo JavaAgent correctly configured, colorize Java source files and show a coverage report.
  *
  * @author Jonathan Lermitage
  * @author Jan Lahoda (patch https://github.com/jonathanlermitage/tikione-jacocoverage/pull/3)
  */
 @ActionID(category = "Project",
-          id = "fr.tikione.jacocoverage.plugin.action.MenuAntRunProject")
-@ActionRegistration(displayName = "#CTL_MenuAntRunProject",
+          id = "fr.tikione.jacocoverage.plugin.action.ShortcutAntRunProject")
+@ActionRegistration(displayName = "#CTL_ShortcutAntRunProject",
                     lazy = false,
                     asynchronous = true,
                     surviveFocusChange = true)
-@ActionReference(path = "Projects/Actions",
-                 position = 1984,
-                 separatorBefore = 1983)
-@NbBundle.Messages("CTL_MenuAntRunProject=Run with JaCoCoverage")
-public class MenuAntRunProject
+@ActionReference(path = "Shortcuts",
+                 name = "F12")
+@NbBundle.Messages("CTL_ShortcutAntRunProject=Run with JaCoCoverage")
+public class ShortcutAntRunProject
         extends JaCoCoActionOnAnt
-        implements ContextAwareAction, LookupListener, Presenter.Popup {
+        implements ContextAwareAction {
 
     private static final long serialVersionUID = 1L;
 
-    public MenuAntRunProject() {
+    public ShortcutAntRunProject() {
         this(Utilities.actionsGlobalContext());
     }
 
-    public MenuAntRunProject(Lookup context) {
+    public ShortcutAntRunProject(Lookup context) {
         super(context, context.lookup(Project.class), "run");
-        setEnabled(Utils.isProjectSupported(getProject()));
-        putValue(DynamicMenuContent.HIDE_WHEN_DISABLED, true);
         putValue(Action.NAME, Bundle.CTL_MenuAntRunProject());
-        putValue(Action.SMALL_ICON, ImageUtilities.loadImageIcon(Globals.RUN_ICON, false));
-        if (isEnabled()) { // Don't try to enable if project's type is not supported.
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        if (Utils.isProjectSupported(getProject())) {
             FileObject prjPropsFo = getProject().getProjectDirectory().getFileObject("nbproject/project.properties");
             final Properties prjProps = new Properties();
             InputStream ins = null;
             try {
                 if (prjPropsFo != null) {
                     prjProps.load(ins = prjPropsFo.getInputStream());
-                }
-                if (prjProps.getProperty("main.class", "").isEmpty()) {
-                    setEnabled(false);
+                    if (!prjProps.getProperty("main.class", "").isEmpty()) {
+                        super.actionPerformed(ev);
+                    }
                 }
             } catch (IOException ex) {
                 Exceptions.printStackTrace(ex);
@@ -83,23 +76,7 @@ public class MenuAntRunProject
     }
 
     @Override
-    public void actionPerformed(ActionEvent ev) {
-        if (isEnabled()) {
-            super.actionPerformed(ev);
-        }
-    }
-
-    @Override
     public Action createContextAwareInstance(Lookup context) {
-        return new MenuAntRunProject();
-    }
-
-    @Override
-    public void resultChanged(LookupEvent ev) {
-    }
-
-    @Override
-    public JMenuItem getPopupPresenter() {
-        return new JMenuItem(this);
+        return new ShortcutAntRunProject();
     }
 }
--- tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/action/ShortcutAntTestProject.java
+++ tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/action/ShortcutAntTestProject.java
@@ -1,76 +1,58 @@
 package fr.tikione.jacocoverage.plugin.action;
 
 import fr.tikione.jacocoverage.plugin.Utils;
-import fr.tikione.jacocoverage.plugin.config.Globals;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
-import javax.swing.JMenuItem;
 import org.netbeans.api.project.Project;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.awt.DynamicMenuContent;
 import org.openide.util.ContextAwareAction;
-import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
-import org.openide.util.LookupEvent;
-import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
-import org.openide.util.actions.Presenter;
 
 /**
- * The "Test with JaCoCoverage" contextual action registration.
+ * The "Test with JaCoCoverage" shortcut action registration.
  * Start the "test" Ant task with the JaCoCo JavaAgent correctly configured, colorize Java source files and show a coverage report.
  *
  * @author Jonathan Lermitage
  */
 @ActionID(category = "Project",
-          id = "fr.tikione.jacocoverage.plugin.action.MenuAntTestProject")
-@ActionRegistration(displayName = "#CTL_MenuAntTestProject",
+          id = "fr.tikione.jacocoverage.plugin.action.ShortcutAntTestProject")
+@ActionRegistration(displayName = "#CTL_ShortcutAntTestProject",
                     lazy = false,
                     asynchronous = true,
                     surviveFocusChange = true)
-@ActionReference(path = "Projects/Actions",
-                 position = 1985,
-                 separatorAfter = 1986)
-@NbBundle.Messages("CTL_MenuAntTestProject=Test with JaCoCoverage")
-public final class MenuAntTestProject
+@ActionReference(path = "Shortcuts",
+                 name = "O-F12")
+@NbBundle.Messages("CTL_ShortcutAntTestProject=Test with JaCoCoverage")
+public final class ShortcutAntTestProject
         extends JaCoCoActionOnAnt
-        implements ContextAwareAction, LookupListener, Presenter.Popup {
+        implements ContextAwareAction {
 
     private static final long serialVersionUID = 1L;
 
-    public MenuAntTestProject() {
+    public ShortcutAntTestProject() {
         this(Utilities.actionsGlobalContext());
     }
 
-    public MenuAntTestProject(Lookup context) {
+    public ShortcutAntTestProject(Lookup context) {
         super(context, context.lookup(Project.class), "test");
-        setEnabled(Utils.isProjectSupported(getProject()));
         putValue(DynamicMenuContent.HIDE_WHEN_DISABLED, true);
-        putValue(Action.NAME, Bundle.CTL_MenuAntTestProject());
-        putValue(Action.SMALL_ICON, ImageUtilities.loadImageIcon(Globals.TEST_ICON, false));
+        putValue(Action.NAME, Bundle.CTL_ShortcutAntTestProject());
     }
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-        if (isEnabled()) {
+        if (Utils.isProjectSupported(getProject())) {
             super.actionPerformed(ev);
         }
     }
 
     @Override
     public Action createContextAwareInstance(Lookup context) {
-        return new MenuAntTestProject(context);
-    }
-
-    @Override
-    public void resultChanged(LookupEvent ev) {
-    }
-
-    @Override
-    public JMenuItem getPopupPresenter() {
-        return new JMenuItem(this);
+        return new ShortcutAntTestProject(context);
     }
 }
