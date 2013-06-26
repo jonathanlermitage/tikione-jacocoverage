5413cb023caab3153dbd876e0ff50b470a434da8
jonathanlermitage <jonathan.lermitage@gmail.com>
2013-06-26 19:38:19 +0200

    fixed spelling Signed-off-by:jonathanlermitage <jonathan.lermitage@gmail.com>

--- tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/config/ProjectConfig.java
+++ tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/config/ProjectConfig.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * Plugin's configuration handler at project's level.
+ * Plugin's configuration handler at project level.
  *
  * @author Jonathan Lermitage
  */
