c639821642de9e75dd6e61f080eb6a9626b77095
jonathanlermitage <jonathan.lermitage@gmail.com>
2013-09-11 13:21:32 +0200

    fix GitHub #16

--- tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/action/JaCoCoActionOnAnt.java
+++ tikione-jacocoverage-plugin/src/fr/tikione/jacocoverage/plugin/action/JaCoCoActionOnAnt.java
@@ -147,6 +147,9 @@ public abstract class JaCoCoActionOnAnt
                         + ",destfile=\"" + binreport.getAbsolutePath() + "\"" + exclude.toString();
 
                 FileObject scriptToExecute = project.getProjectDirectory().getFileObject("build", "xml");
+                if (scriptToExecute == null) { // Fix for GitHub #16
+                    scriptToExecute = project.getProjectDirectory().getFileObject("nbbuild", "xml");
+                }
                 DataObject dataObj = DataObject.find(scriptToExecute);
                 AntProjectCookie antCookie = dataObj.getLookup().lookup(AntProjectCookie.class);
 
